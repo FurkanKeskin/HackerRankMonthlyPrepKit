fc1711db6bad3f175cc7eabbfc855cd6b4004eda
Furkan Keskin <furkan-keskin@yandex.com>
2022-02-22 14:26:07 +0300

    Create MiniMaxSum.js
    O(n) solution with JS

---
+++ MiniMaxSum.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    const sortedArr = arr.sort();
+    const totalSum = sortedArr.reduce(
+        (prev, curr) => prev + curr, 0
+    );
+    const min = totalSum - sortedArr[4];
+    const max = totalSum - sortedArr[0];
+    console.log(min, max);
+    return;
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
