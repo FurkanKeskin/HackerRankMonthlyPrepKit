9817ad3a41994097f66fb46dea28b52ddfe2cdf4
Furkan Keskin <furkan-keskin@yandex.com>
2022-02-22 04:58:36 +0300

    Update PlusMinus.js
    There should be a better solution than O(3n)

--- PlusMinus.js
+++ PlusMinus.js
@@ -28,11 +28,18 @@ function readLine() {
 
 function plusMinus(arr) {
     // const sortedArr = arr.sort(); // This sorted array will be here for further experiments
-    const counts = [];
-        counts.positives = arr.filter(i => i > 0).length;
-        counts.negatives = arr.filter(i => i < 0).length;
-        counts.zeros =  arr.filter(i => i === 0).length;
-        
+    const counts = [{
+        positives: 0,
+        negatives: 0,
+        zeros: 0
+    }];
+    counts.positives = arr.filter(i => i > 0).length;
+    counts.negatives = arr.filter(i => i < 0).length;
+    counts.zeros =  arr.filter(i => i === 0).length;
+    
+    // Above creation of the counts array of objects has O(3n)
+    // time complexity since I've used arr.filter for three conditions
+    // there should be some other more elegant solution..
     console.log((counts.positives / arr.length).toFixed(5));
     console.log((counts.negatives / arr.length).toFixed(5));
     console.log((counts.zeros / arr.length).toFixed(5));
